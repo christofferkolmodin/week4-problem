a3c573b4090d291c4955b9da836d94c86c422399
christofferkolmodin <kolmodin.chris@gmail.com>
2025-09-28 01:05:07 +0200

    first draft elastic collision

--- src/bouncing_balls/Model.java
+++ src/bouncing_balls/Model.java
@@ -34,19 +34,20 @@ class Model {
 
 			if (isCollisionWithWall(ball)) {
 				changeHorizontalDirection(ball);
+				System.out.println("Horizontal velocity after wall collision: " + ball.vx);
 			}
 
 			if (isCollisionWithFloor(ball)) {
 				handleFloorCollision(ball);
-				System.out.println("Velocity after collision with floor: " + ball.vy);
+//				System.out.println("Velocity after collision with floor: " + ball.vy);
 
 			} else if (isCollisionWithRoof(ball)) {
 				handleRoofCollision(ball);
-				System.out.println("Velocity after collision with roof: " + ball.vy);
+//				System.out.println("Velocity after collision with roof: " + ball.vy);
 			}
 
 			applyGravity(ball, deltaT);
-			System.out.println("After calculation velocity is: " + ball.vy);
+//			System.out.println("After calculation velocity is: " + ball.vy);
 			// compute new position according to the speed of the ball
 			updatePosition(ball, deltaT);
 		}
@@ -80,7 +81,60 @@ class Model {
 	}
 
 	private void bounceTimeBaby(Ball ball1, Ball ball2) {
-		conservationOfEnergy(ball1, ball2);
+		preventBallsOverlapping(ball1, ball2);
+		handleElasticCollision(ball1, ball2);
+	}
+
+	private void preventBallsOverlapping(Ball ball1, Ball ball2) {
+		double overlapAmount = (ball1.radius + ball2.radius) - distanceBetweenBalls(ball1, ball2);
+		double amountMove = overlapAmount / 2;
+
+		separateInDirectionOfMovement(ball1, ball2, amountMove);
+	}
+
+	private void separateInDirectionOfMovement(Ball ball1, Ball ball2, double amountMove) {
+		double normalX = xDistanceBetweenBalls(ball1, ball2) / distanceBetweenBalls(ball1, ball2);
+		double normalY = yDistanceBetweenBalls(ball1, ball2) / distanceBetweenBalls(ball1, ball2);
+
+		if (ball1OnLeft(ball1, ball2)) {
+			fixPositionWhenBall1Left(ball1, ball2, amountMove, normalX);
+		} else {
+			fixPositionWhenBall2Left(ball1, ball2, amountMove, normalX);
+		}
+
+		if (ball1OnTop(ball1, ball2)) {
+			fixPositionWhenBall1Top(ball1, ball2, amountMove, normalY);
+		} else {
+			fixPositionWhenBall2Top(ball1, ball2, amountMove, normalY);
+		}
+	}
+
+	private void fixPositionWhenBall1Left(Ball ball1, Ball ball2, double amountMove, double normalX) {
+		ball1.x -= (normalX * amountMove);
+		ball2.x += (normalX * amountMove);
+	}
+
+	private void fixPositionWhenBall2Left(Ball ball1, Ball ball2, double amountMove, double normalX) {
+		ball2.x -= (normalX * amountMove);
+		ball1.x += (normalX * amountMove);
+	}
+
+	private void fixPositionWhenBall1Top(Ball ball1, Ball ball2, double amountMove, double normalY) {
+		ball1.y += (normalY * amountMove);
+		ball2.y -= (normalY * amountMove);
+	}
+
+	private void fixPositionWhenBall2Top(Ball ball1, Ball ball2, double amountMove, double normalY) {
+		ball2.y += (normalY * amountMove);
+		ball1.y -= (normalY * amountMove);
+	}
+
+	private boolean ball1OnLeft(Ball ball1, Ball ball2) {
+		return ball1.x < ball2.x;
+	}
+
+	private boolean ball1OnTop(Ball ball1, Ball ball2) {
+		return ball1.y > ball2.y;
 	}
 
 	private double distanceBetweenBalls(Ball ball1, Ball ball2) {
@@ -89,16 +143,56 @@ class Model {
 		return Math.sqrt( square(ball2.x - ball1.x) + square(ball2.y - ball1.y)  );
 	}
 
-	private void conservationOfEnergy(Ball ball1, Ball ball2) {
-		// TODO: Fix this implementation
+	private double xDistanceBetweenBalls(Ball ball1, Ball ball2) {
+		return Math.abs(ball2.x - ball1.x);
+	}
 
-		//	m1v1 + m2v2 = I
-		double momentumX = ball1.weight * ball1.vx + ball2.weight * ball2.vx;
-		double momentumY = ball1.weight * ball1.vy + ball2.weight * ball2.vy;
+	private double yDistanceBetweenBalls(Ball ball1, Ball ball2) {
+		return Math.abs(ball2.y - ball1.y);
+	}
 
-		//	v2 − v1 = −R
+	private void handleElasticCollision(Ball ball1, Ball ball2) {
+		// TODO: Tangential velocity unchanged?
+
+		/*
+			"u" represents velocity before collision, "v" represents velocity after collision
+		*/
+
+		// Conservation of momentum before collision
+		// I = m1u1 + m2u2
+		double momentumX = (ball1.weight * ball1.vx) + (ball2.weight * ball2.vx);
+		double momentumY = (ball1.weight * ball1.vy) + (ball2.weight * ball2.vy);
+
+		// Conservation of energy before collision
+		// R = u2 − u1
 		double relativeVelocityX = ball2.vx - ball1.vx;
 		double relativeVelocityY = ball2.vy - ball1.vy;
+
+		/* System of equations for after collision */
+		//	(1) m1v1 + m2v2 = I
+		//	(2) v2 − v1 = −R
+		//  	v2 = v1 - R
+		//  insert v2 into (1)
+		//  	m1v1 + (m2(v1 - R)) = I
+		//  	m1v1 + m2v1 - m2*R = I
+		//  	v1(m1 + m2) - m2*R = I
+
+		//  So,
+		//  	v1 = I + m2*R / (m1 + m2)
+		//  insert v1 into (2) and solve for v2
+		//  	v2 = (I + m2*R / (m1 + m2)) - R
+		//		v2 = I + m2*R - R(m1 + m2) / (m1 + m2)
+		//		v2 = I - m1*R / (m1 + m2)
+
+		double sumWeights = ball1.weight + ball2.weight;
+
+		// v1 = I + m2*R / (m1 + m2)
+		ball1.vx = (momentumX + (ball2.weight*relativeVelocityX)) / sumWeights;
+		ball1.vy = (momentumY + (ball2.weight*relativeVelocityY)) / sumWeights;
+
+		// v2 = I - m1*R / (m1 + m2)
+		ball2.vx = (momentumX - (ball1.weight * relativeVelocityX)) / sumWeights;
+		ball2.vy = (momentumY - (ball1.weight * relativeVelocityY)) / sumWeights;
 	}
 
 	private double square(double value) {
